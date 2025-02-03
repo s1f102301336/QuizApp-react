dedc0174be80443bc57adb66be3e73b2b170a9a1
iniad <s1f102301336@iniad.org>
2025-02-03 16:13:32 +0900

    2人そろうとゲームスタートを実装

--- src/app/quiz/multiplayer/[id]/Start.tsx
+++ src/app/quiz/multiplayer/[id]/Start.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
 export const Start = () => {
-  const delDoc = () => {};
   return (
     <div>
       <div>Start</div>
-      <div>
-        <button onClick={delDoc}>終了</button>
-      </div>
     </div>
   );
 };
--- src/app/quiz/multiplayer/[id]/page.tsx
+++ src/app/quiz/multiplayer/[id]/page.tsx
@@ -2,35 +2,48 @@
 
 import { rtdb } from "@/firebase";
 import { useParams } from "next/navigation";
-import { ref, set, update, get } from "firebase/database";
+import {
+  ref,
+  set,
+  update,
+  get,
+  remove,
+  onValue,
+  off,
+  DataSnapshot,
+} from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { Header } from "@/components/Header";
 import { Start } from "./Start";
 
 const Multiplayer = () => {
   const params = useParams();
-  const [ready, setReady] = useState(false);
+  const roomId = params.id as string;
+  const [roomData, setRoomData] = useState(null);
+
+  const ready = roomData && "user2" in roomData;
 
   useEffect(() => {
-    const roomId = params.id as string;
-    const dbRef = ref(rtdb, `rooms/${roomId}`); //roomsコレクションのもとにroomIdドキュメントを作成
-    console.log("Hello");
+    const roomRef = ref(rtdb, `rooms/${roomId}`); //roomsコレクションのもとにroomIdドキュメントを作成
 
+    if (!roomId) return;
+
+    //部屋を作成・2人目は更新して参加
     const createRoom = async () => {
       try {
-        const roomSnapshot = await get(dbRef);
+        const roomSnapshot = await get(roomRef);
         console.log(roomSnapshot.val());
-        console.log("Hello2");
+
         if (!roomSnapshot.exists()) {
           //フィールド値を作成
-          await set(dbRef, {
+          await set(roomRef, {
             roomId: roomId,
             user1: {
               id: 1234,
               name: "Taro",
             },
           });
-          console.log("data", roomSnapshot.val());
+          console.log("data", (await get(roomRef)).val());
         } else {
           const newUser = {
             user2: {
@@ -38,25 +51,52 @@ const Multiplayer = () => {
               name: "John",
             },
           };
-          await update(dbRef, newUser);
-          setReady(true);
-          console.log("data", roomSnapshot.val());
+          await update(roomRef, newUser);
+          console.log("data", (await get(roomRef)).val());
         }
       } catch (error) {
         console.error("Error", error);
       }
     };
     createRoom();
-  }, [params.id]);
+
+    //リスナーを登録（更新を常に監視できるようになる）
+    const callback = (snapshot: DataSnapshot) => {
+      if (snapshot.exists()) {
+        console.log("Data updated", snapshot.val());
+        setRoomData(snapshot.val());
+      } else {
+        setRoomData(null);
+      }
+    };
+    onValue(roomRef, callback);
+
+    //クリーンアップ関数
+    return () => off(roomRef, "value", callback);
+  }, [roomId]);
+
+  //部屋のドキュメントを削除する関数
+  const delDoc = async () => {
+    const roomRef = ref(rtdb, `rooms/${params.id}`);
+    await remove(roomRef);
+    return;
+  };
 
   return (
     <div>
       <Header />
+      <div>
+        roomData:{roomData ? JSON.stringify(roomData, null, 2) : "No Data"}
+      </div>
       {ready && (
         <div>
           <Start />
         </div>
       )}
+
+      <div>
+        <button onClick={delDoc}>部屋を解散する</button>
+      </div>
     </div>
   );
 };
