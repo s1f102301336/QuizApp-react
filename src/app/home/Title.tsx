a4ef7c1060d5b3287b8824af4aa14ab6156c25d4
iniad <s1f102301336@iniad.org>
2025-03-14 13:15:58 +0900

    HeaderとFooter以外のデザインを作成

--- src/app/home/Title.tsx
+++ src/app/home/Title.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import style from "./Title.module.css";
 import { Form } from "@/components/Form";
+import Image from "next/image";
+import Logo from "../../../public/Logo_2.png";
+import { Jost } from "next/font/google";
+
+const jost = Jost({ subsets: ["latin"], weight: ["700"] });
 
 export const Title = () => {
   return (
     <div className={style.container}>
-      <div>Logo</div>
-      <div>Quiz Dash</div>
-      <Form />
+      <div className={style.body}>
+        <div className={style.text}>
+          <Image src={Logo} alt="Logo Icon" className={style.logo} />
+          <div className={`${jost.className} ${style.appName}`}>Quiz Dash</div>
+        </div>
+        <Form />
+      </div>
     </div>
   );
 };
