8a226136aab6b92a67d126b2aff24b105853055c
iniad <s1f102301336@iniad.org>
2025-04-27 23:14:23 +0900

    色を統一

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-// import Image from "next/image";
-// import { Header } from "@/components/Header";
 import { QuizContainer } from "./home/QuizContainer";
-import styles from "../styles/page.module.css";
-// import { Footer } from "@/components/Footer";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <QuizContainer />
-      </main>
+    <div>
+      <QuizContainer />
     </div>
   );
 }
